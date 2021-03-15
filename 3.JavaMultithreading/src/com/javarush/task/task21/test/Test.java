ff1d1d6d387a93cf6d3ed07fa2e1579c37202e00
Shemetov Andrey <guruspirit@mail.ru>
2021-03-15 19:41:41 +0300

    commit message 15.03.21 перед решением занади Рефакторинг

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/test/Test.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task21.test;
+
+public class Test {
+    public static void main(String[] args) {
+        //Integer i = Integer.valueOf("11",2);
+        //System.out.println(i);
+
+    }
+}
